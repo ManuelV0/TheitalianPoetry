2cc17d25be97217c4e29f152c9af44219fa6d306
ManuelV0 <manuel.voll69@icloud.com>
2025-08-03 00:40:48 +0200

    Update index.tsx

--- WidgetAPP/src/index.tsx
+++ WidgetAPP/src/index.tsx
@@ -5,13 +5,19 @@ import App from './App'
 const mount = (el: HTMLElement) => {
   const root = ReactDOM.createRoot(el)
   root.render(<App />)
-
   return () => root.unmount()
 }
 
+// Solo per sviluppo, non fa nulla in produzione:
 if (process.env.NODE_ENV === 'development') {
   const devRoot = document.getElementById('root')
   if (devRoot) mount(devRoot)
+}
+
+// AGGIUNGI QUESTO!
+if (typeof window !== "undefined") {
+  // @ts-ignore
+  window.MyPoetryApp = { mount }
 }
 
 export { mount }
