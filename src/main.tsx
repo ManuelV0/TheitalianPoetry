888ec6a765bb717b6bf5162e0e42635954733ab7
ManuelV0 <manuel.voll69@icloud.com>
2025-08-03 18:30:49 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
+// Funzione che monta il widget su un elemento DOM passato
+function mount(element: HTMLElement) {
+  ReactDOM.createRoot(element).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+// Esposizione globale: window.MyPoetryApp.mount
+if (typeof window !== "undefined") {
+  (window as any).MyPoetryApp = { mount };
+}
+
+export { mount }; // Utile per eventuali test o import interni
