7ecfc424aaff52cb4ab7afe5b5fac21fb3375eb5
ManuelV0 <manuel.voll69@icloud.com>
2025-08-03 16:27:57 +0200

    Update vite.config.ts

--- WidgetAPP/vite.config.ts
+++ WidgetAPP/vite.config.ts
@@ -1,11 +1,16 @@
-// vite.config.ts
+import { defineConfig } from 'vite'  // Assicurati che questo import sia presente
+import react from '@vitejs/plugin-react'
+
+// Verifica che questa riga sia presente
 export default defineConfig({
+  plugins: [react()],
   build: {
+    outDir: 'dist',
     lib: {
       entry: 'src/index.tsx',
       name: 'MyPoetryApp',
-      formats: ['iife'],
-      fileName: 'my-poetry-app'
+      fileName: 'my-poetry-app',
+      formats: ['iife']
     },
     rollupOptions: {
       external: ['react', 'react-dom'],
@@ -13,9 +18,7 @@ export default defineConfig({
         globals: {
           react: 'React',
           'react-dom': 'ReactDOM'
-        },
-        // Garantisce l'esportazione globale
-        footer: 'window.MyPoetryApp = { mount: default };'
+        }
       }
     }
   }
