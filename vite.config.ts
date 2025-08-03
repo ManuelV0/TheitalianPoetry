0db859e2b3b0cd711d372fa93cb3471561c473f1
ManuelV0 <manuel.voll69@icloud.com>
2025-08-03 18:23:39 +0200

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '/src': path.resolve(__dirname, 'src'), // Optional: solo se vuoi usare /src nei tuoi import
+    },
+  },
+})
