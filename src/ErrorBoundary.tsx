b58a1c4b6d36d9d84a3d8e52fcc94fb370628055
ManuelV0 <manuel.voll69@icloud.com>
2025-08-06 14:33:37 +0200

    Update ErrorBoundary.tsx

--- src/ErrorBoundary.tsx
+++ src/ErrorBoundary.tsx
@@ -1,8 +1,7 @@
-// src/ErrorBoundary.tsx
 import React from "react";
 
 export class ErrorBoundary extends React.Component<{children: React.ReactNode}, {error: any}> {
-  constructor(props) {
+  constructor(props: any) {
     super(props);
     this.state = { error: null };
   }
@@ -12,8 +11,19 @@ export class ErrorBoundary extends React.Component<{children: React.ReactNode}, 
   render() {
     if (this.state.error) {
       return (
-        <div style={{color:'red',background:'#111',padding:'1em'}}>
-          Errore Widget: {String(this.state.error)}
+        <div style={{color:'red',background:'#111',padding:'1em', whiteSpace: 'pre-wrap'}}>
+          <div><b>Errore Widget:</b></div>
+          <div>{String(this.state.error)}</div>
+          {this.state.error && this.state.error.stack && (
+            <details style={{marginTop:8}}>
+              <summary>Stack trace</summary>
+              <pre>{this.state.error.stack}</pre>
+            </details>
+          )}
+          <div style={{fontSize:'0.9em',marginTop:'1em'}}>
+            Se vedi solo "Error" senza dettagli, significa che una fetch o funzione ha lanciato un errore generico.<br/>
+            <b>Segnala questo testo allo sviluppatore!</b>
+          </div>
         </div>
       );
     }
