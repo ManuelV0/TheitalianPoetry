d6224ed3659da60a7370ad0b3a9b72e8322e8489
ManuelV0 <manuel.voll69@icloud.com>
2025-10-03 18:23:34 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist',
     rollupOptions: {
-      external: ['react-icons']
-    },
-    minify: 'terser',
-    emptyOutDir: true,
-  },
-  server: {
-    port: 3000,
-    open: true
+      external: ['react-icons/fa']
+    }
   }
 });
