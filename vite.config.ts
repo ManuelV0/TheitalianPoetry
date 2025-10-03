7b210afd2797814d558db836979657907b645e10
ManuelV0 <manuel.voll69@icloud.com>
2025-10-03 18:21:54 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
-    lib: {
-      entry: path.resolve(__dirname, 'src/index.tsx'), // Punto di ingresso
-      name: 'MyPoetryApp', // Nome globale per IIFE
-      formats: ['iife'],   // Uscita come IIFE (self-executing for browser/widget)
-      fileName: () => 'my-poetry-app.iife.js', // Nome file finale
-    },
     rollupOptions: {
-      output: {
-        globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM',
-        }
-      },
-      external: ['react', 'react-dom'],
+      external: ['react-icons']
     },
     minify: 'terser',
     emptyOutDir: true,
+  },
+  server: {
+    port: 3000,
+    open: true
   }
 });
