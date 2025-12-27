8663c84c043d9a8776513c69e842ee2d1fafc90c
ManuelV0 <manuel.voll69@icloud.com>
2025-12-27 15:36:11 +0100

    Create lib/supabaseClient.ts

---
+++ lib/supabaseClient.ts
@@ -0,0 +1,17 @@
+// src/lib/supabaseClient.ts
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+if (!supabaseUrl || !supabaseAnonKey) {
+  console.error('❌ Supabase env mancanti', {
+    supabaseUrl,
+    supabaseAnonKey,
+  });
+}
+
+export const supabase = createClient(
+  supabaseUrl,
+  supabaseAnonKey
+);
